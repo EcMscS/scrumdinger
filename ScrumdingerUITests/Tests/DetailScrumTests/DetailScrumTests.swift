bce2e185dcb3d42b60b5a1bb8c3fa419c54ded6b
Matt Heaney <main@mattheaney.dev>
2025-02-28 21:03:15 +0000

    Deleted the unneeded test

--- ScrumdingerUITests/Tests/DetailScrumTests/DetailScrumTests.swift
+++ ScrumdingerUITests/Tests/DetailScrumTests/DetailScrumTests.swift
@@ -8,11 +8,6 @@ import XCTest
 
 class DetailScrumTests: XCTestCase {
 
-    func testNavigateBack() {
-        AppRobot()
-            .launchAppWithNewScrum()
-    }
-
     func testScrumDetailsInfo() {
         AppRobot()
             .launchAppWithNewScrum()
