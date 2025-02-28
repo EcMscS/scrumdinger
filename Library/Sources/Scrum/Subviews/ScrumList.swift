5e17da341e4c39a2606efb36d7fcc83a7e8e3513
James Sedlacek <Jsedlacekjr@gmail.com>
2025-02-27 22:59:37 -0500

    Removed error presentation from ScrumList

--- Library/Sources/Scrum/Subviews/ScrumList.swift
+++ Library/Sources/Scrum/Subviews/ScrumList.swift
@@ -15,12 +15,7 @@ import ViewComponents
 struct ScrumList {
     @Environment(\.fileService) private var fileService
     @StoredData private var dailyScrums: [DailyScrum]
-    @State private var errorToPresent: FileServiceError? = nil
     private let cardTapAction: (DailyScrum) -> Void
-
-    private var isErrorPresented: Binding<Bool> {
-        .constant(errorToPresent != nil)
-    }
 
     init(cardTapAction: @escaping (DailyScrum) -> Void) {
         self.cardTapAction = cardTapAction
@@ -30,11 +25,8 @@ struct ScrumList {
         do throws(FileServiceError) {
             dailyScrums = try fileService.load(forKey: .dailyScrumsKey)
         } catch {
-            switch error {
-            case .fileReadFailed(let error):
-                print(error.localizedDescription)
-            default:
-                errorToPresent = error
+            if let description = error.errorDescription {
+                print(description)
             }
         }
     }
@@ -45,12 +37,6 @@ extension ScrumList: View {
         List(dailyScrums, rowContent: scrumRow)
             .refreshable(action: loadData)
             .onAppear(perform: loadData)
-            .alert(
-                isPresented: isErrorPresented,
-                error: errorToPresent,
-                actions: errorActions,
-                message: errorMessage
-            )
     }
 
     private func scrumRow(_ scrum: DailyScrum) -> some View {
@@ -60,13 +46,5 @@ extension ScrumList: View {
             .onTapGesture {
                 cardTapAction(scrum)
             }
-    }
-
-    private func errorMessage(for error: LocalizedError) -> some View {
-        Text(error.errorDescription ?? "Unknown error")
-    }
-
-    private func errorActions(for error: LocalizedError) -> some View {
-        Button(.ok) {}
     }
 }
